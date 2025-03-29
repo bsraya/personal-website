4c10e5e06ab27862ae3e6d88241a44d4c0f60c3a
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-03-29 11:59:46 +0800

    feat: include plugins for mdx
    1. Show mathematical equations
    2. Enable Table of Contents
    3. Minify mdx -> html size

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,21 @@ import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkToc from "remark-toc";
+import rehypePresetMinify from "rehype-preset-minify";
 
 export default defineConfig({
   integrations: [
-    mdx(),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      remarkPlugins: [remarkMath, remarkToc],
+      rehypePlugins: [rehypeKatex, rehypePresetMinify],
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: true,
+    }),
     solidJs({ devtools: true })
   ],
   vite: {
