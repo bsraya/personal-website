3bbadef0f41ac3a10a10c23e8ab5fcda2fe71d2c
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-03-29 11:58:25 +0800

    feat: include tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [
     mdx(),
     solidJs({ devtools: true })
-  ]
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  }
 });
