108e7e318a1f8d0d7e6f01aa492ff727384f6cc1
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-04-05 23:30:37 +0800

    feat: improve burger menu with fallback

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -1,44 +1,24 @@
-import { createSignal, onMount, onCleanup } from "solid-js";
 import { FiMenu } from 'solid-icons/fi';
+import { Show, createSignal, onMount, onCleanup } from "solid-js";
+import clickOutside from "../utils/click-outside";
 
 export default function BurgerMenuSolid() {
   const [show, setShow] = createSignal(false);
-  let dropdownRef: HTMLDivElement;
-
-  const handleDocumentClick = (e: MouseEvent) => {
-    if (dropdownRef && !dropdownRef.contains(e.target as Node)) {
-      setShow(false);
-    }
-  }
-
-  const toggleMenu = (e: MouseEvent) => {
-    setShow(!show());
-  };
-
-  onMount(() => {
-    document.addEventListener("click", handleDocumentClick);
-
-    onCleanup(() => {
-      document.removeEventListener("click", handleDocumentClick);
-    });
-  });
+  
+  const onClick = () => setShow(!show());
 
   return (
-    <div class="relative grid grid-col z-40" ref={dropdownRef}>
-      <button
-        class="w-fit ml-auto cursor-pointer"
-        onClick={toggleMenu}
-      >
-        <FiMenu size={24} />
-      </button>
-      {
-        show() &&
-          <ul class="absolute top-full right-0 mt-2 w-48 bg-white shadow-lg rounded-md border border-gray-200 transform -translate-x-0">
-            <li><a href="/" class="block px-4 py-2 hover:bg-gray-100">Home</a></li>
-            <li><a href="/posts" class="block px-4 py-2 hover:bg-gray-100">Posts</a></li>
-            <li><a href="/works" class="block px-4 py-2 hover:bg-gray-100">Works</a></li>
-          </ul>
-      }
-    </div>
+    <Show when={show()} fallback={<button onClick={onClick} class="w-fit ml-auto cursor-pointer"><FiMenu size={24} /></button>}>
+      <div class="relative grid grid-col z-40" use:clickOutside={onClick}>
+        <button onClick={onClick} class="w-fit ml-auto cursor-pointer">
+          <FiMenu size={24} />
+        </button>
+        <ul  class="absolute top-full right-0 mt-2 w-48 bg-white shadow-lg rounded-md border border-gray-200 transform -translate-x-0">
+          <li><a href="/" class="block px-4 py-2 hover:bg-gray-100">Home</a></li>
+          <li><a href="/posts" class="block px-4 py-2 hover:bg-gray-100">Posts</a></li>
+          <li><a href="/works" class="block px-4 py-2 hover:bg-gray-100">Works</a></li>
+        </ul>
+      </div>
+    </Show>
   )
 }
---
+++ src/utils/click-outside.tsx
@@ -0,0 +1,21 @@
+import { onCleanup } from "solid-js";
+
+declare module "solid-js" {
+  namespace JSX {
+    interface Directives {
+      clickOutside: () => void;
+    }
+  }
+}
+
+export default function clickOutside(el: HTMLElement, accessor: () => (() => void)) {
+  const handler = (e: MouseEvent) => {
+    if (!el.contains(e.target as Node)) {
+      const cb = accessor();
+      if (typeof cb === 'function') cb();
+    }
+  };
+
+  document.addEventListener("click", handler);
+  onCleanup(() => document.removeEventListener("click", handler));
+}
