715ba1f1ff5531b7b596fc40ba30b40a3d507647
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-04-27 11:41:10 +0800

    style: add paddings

--- src/components/TableOfContent.tsx
+++ src/components/TableOfContent.tsx
@@ -8,7 +8,7 @@ export default function TableOfContent({ url, headings }: { url: string, heading
   const toggleToc = () => setTocOpen(!tocOpen());
 
   return (
-    <div class={`p-3 fixed bottom-5 left-1/2 -translate-x-1/2 z-20 w-[250px] border-2 rounded cursor-pointer bg-white`} onClick={toggleToc}>
+    <div class={`px-3 py-1 fixed bottom-5 left-1/2 -translate-x-1/2 z-20 w-[250px] border-2 rounded cursor-pointer bg-white`} onClick={toggleToc}>
       <button class="flex w-full font-semibold cursor-pointer">
         <span>Table of Content</span>
         <span class="my-auto ml-auto">
@@ -18,12 +18,12 @@ export default function TableOfContent({ url, headings }: { url: string, heading
         </span>
       </button>
       <Show when={tocOpen()}>
-        <ul class="list-disc flex flex-col gap-1 mt-2" use:clickOutside={toggleToc}>
+        <ul class="toc list-disc flex flex-col gap-1 mt-2" use:clickOutside={toggleToc}>
           <For each={headings}>
             {
               (heading) => (
                 <li
-                  class={`${heading.depth === 2 ? 'ml-[1rem] font-semibold' : heading.depth === 3 ? 'ml-[1.75rem]': heading.depth === 4 ? 'ml-[2.25rem]': ''}`}
+                  class={`${heading.depth === 2 ? 'ml-[1rem] font-semibold' : heading.depth === 3 ? 'ml-[2rem]': heading.depth === 4 ? 'ml-[3rem]': ''}`}
                 >
                   <a href={`${url}#${heading.slug}`} class="hover:underline custor-pointer">
                     {heading.text}
