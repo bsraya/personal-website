defc1477994f5a40bb6f4e1ca9a6b5d4577d9b7b
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-05-02 17:28:48 +0800

    others: remove unneeded files

--- src/global.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="astro/client" />
-/// <reference types="vite-plugin-pwa/vanillajs" />
-/// <reference types="vite-plugin-pwa/solid" />
-/// <reference types="vite-plugin-pwa/info" />
