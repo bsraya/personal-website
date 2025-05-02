d566de3a409634abed00e45ff6e870e2d6660fd5
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-05-02 23:35:39 +0800

    style: change og text and bg color

--- src/pages/[slug].og.ts
+++ src/pages/[slug].og.ts
@@ -11,7 +11,7 @@ export async function GET({ params }: APIContext) {
 
     const markup = html(`
     <div
-        style="height: 100%; width: 100%; display: flex; flex-direction: column; justify-content: space-between; align-items: flex-start; background-color: rgb(45,26,84); color: white; padding: 50px; font-family: 'Inter'; "
+        style="height: 100%; width: 100%; display: flex; flex-direction: column; justify-content: space-between; align-items: flex-start; background-color: rgb(201, 219, 255); color: rgb(42, 65, 177); padding: 50px; font-family: 'Share Tech Mono'; "
     >
         <div style="display: flex; align-items: center;">
             <img src="https://bsraya.com/static/icon-96x96.png" alt="icon" style="width: 50px; height: 50px; margin-right: 20px;" />
