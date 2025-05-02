4114276734d01cfddb412e8e401567488d66e18d
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-05-02 14:15:44 +0800

    style: set fill color and size

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -12,7 +12,7 @@ export default function BackToTop() {
       onClick={scrollToTopFn}
       class="fixed bottom-5 right-5 z-10 p-2 bg-white border-2 rounded cursor-pointer"
     >
-      <BsTriangleFill color="#000000" />
+      <BsTriangleFill color="#000000" fill="#000000" size={16} />
     </button>
   )
 }
