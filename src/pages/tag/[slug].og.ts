5a4263828832afb349386716d8a9313b556643c0
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2026-02-22 18:51:32 +0800

    fix: convert png buffer to Uint8Array for response

--- src/pages/tag/[slug].og.ts
+++ src/pages/tag/[slug].og.ts
@@ -42,7 +42,7 @@ export async function GET({ params }: APIContext) {
     const png = await sharp(Buffer.from(svg)).png().toBuffer()
 
     return new Response(
-        png,
+        new Uint8Array(png),
         {
             status: 200,
             headers: {
